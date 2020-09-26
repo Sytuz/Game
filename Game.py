68e1de94caaedcb93ee85046e18d78361ff02f81
Sytuz <alexandreribeiro2077@gmail.com>
2020-09-26 01:53:42 +0100

    Finish the hard part of the game (user input). Make some tweaks to the code

--- Game.py
+++ Game.py
@@ -1,14 +1,19 @@
 import time
 import sys
 import os
+from Player import Player
 
+def clear(lines):
+    for i in range(lines):
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
+        sys.stdout.flush()
 print("""
------------------------------
+---------------------------------------------
 Welcome to my test game!
-This game is based on the
-Cyberpunk universe.
+This game is based on the Cyberpunk universe.
 I hope you enjoy it!
------------------------------
+---------------------------------------------
 """)
 
 time.sleep(3)
@@ -16,23 +21,17 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 loop = 0
 while loop == 0:
-    print("""
------------------------------
+    print("""------------------------------------------------------------------------
 What's your street name?
     
-Think wisely, the name you
-choose here can't be changed
-during the game.
------------------------------
+Think wisely, the name you choose here can't be changed during the game.
+------------------------------------------------------------------------
 """)
 
     name = input().strip()
 
     print(f"""
------------------------------
-Are you sure you want your
-name to be {name}?
------------------------------
+Are you sure you want your name to be {name}?
 """)
 
     while True:
@@ -45,21 +44,76 @@ name to be {name}?
             break
         else:
             print("""
------------------------------
+------------------------
 Please answer yes or no.
------------------------------
+------------------------
 """)
             time.sleep(1)
-            for i in range(6):
-                sys.stdout.write("\033[F")
-                sys.stdout.write("\033[K")
-                sys.stdout.flush()
+            clear(6)
             continue
-        os.system('cls' if os.name == 'nt' else 'clear')
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+attr_points = 8
+attr = ["body", "intelligence", "reflexes", "technical", "cool", "bod", "int", "ref", "tech",]
+
+player = Player(name)
+
+while True:
+
+    print(f"""-----------------------------------------------------------------------
+Please customize your characters' attributes.
+You have {attr_points} points left.
+to spend.
+- Body: {player.bod}
+- Intelligence: {player.int}
+- Reflexes: {player.ref}
+- Technical: {player.tech}
+- Cool: {player.cool}
+(Type the attribute you want to increase and the value you want to add)
+-----------------------------------------------------------------------
+""")
+    if attr_points == 0:
+        break
+
+    while True:
+        answer = input().lower().strip().split()
+        if len(answer) == 2 and answer[0] in attr:
+            try:
+                answer[1] = int(answer[1])
+                if answer[1] <= attr_points and answer[1] > 0:
+                    if player.see_change(answer) <= 10:
+                        player.change(answer)
+                        attr_points -= answer[1]
+                        break
+                    else:
+                        print("Each attribute has a maximum value of 10!")
+                        time.sleep(2)
+                        clear(2)
+                else:
+                    print("That number is too big, please try another one.")
+                    time.sleep(2)
+                    clear(2)
+
+            except ValueError:
+                print("Invalid input, please type a number following the attribute!")
+                time.sleep(2)
+                clear(2)
+        else:
+            print("Invalid input, please follow the instructions.")
+            time.sleep(2)
+            clear(2)
+            
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
-print("""
------------------------------
+
+
+time.sleep(3)
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+print("""-----------------------------
 Before we get started, choose
 your background.
 -----------------------------
--- Player.py
+++ Player.py
@@ -24,3 +24,37 @@ class Player:
 
     def get_cool(self):
         return self.cool
+
+    def get_all_attr(self):
+        return self.bod, self.int, self.ref, self.tech, self.cool
+
+    def change(self, lst):
+        if lst[0] == "bod" or lst[0] == "body":
+            self.bod += lst[1]
+            value = self.bod
+        elif lst[0] == "intelligence" or lst[0] == "int":
+            self.int += lst[1]
+            value = self.int
+        elif lst[0] == "reflexes" or lst[0] == "ref":
+            self.ref += lst[1]
+            value = self.ref
+        elif lst[0] == "tech":
+            self.tech += lst[1]
+            value = self.tech
+        elif lst[0] == "cool":
+            self.cool += lst[1]
+            value = self.cool
+        return value
+
+    def see_change(self, lst):
+        if lst[0] == "bod" or lst[0] == "body":
+            value = self.bod + lst[1]
+        elif lst[0] == "intelligence" or lst[0] == "int":
+            value = self.int + lst[1]
+        elif lst[0] == "reflexes" or lst[0] == "ref":
+            value = self.ref + lst[1]
+        elif lst[0] == "tech":
+            value = self.tech + lst[1]
+        elif lst[0] == "cool":
+            value = self.cool + lst[1]
+        return value
